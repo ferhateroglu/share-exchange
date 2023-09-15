8fb804a23f0413f0377c5580a37099b8a023013a
Ferhat Eroğlu <erogluferhat@outlook.com>
2023-09-16 01:18:49 +0300

    moved server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Application } from "express";
-import Server from "./src/index";
+import Server from "./index";
 
 const app: Application = express();
 const server: Server = new Server(app);
